7d0533ee1364914268dc1b7cbbc187554dfce514
Ayban <mrkvnhrnndz17@gmail.com>
2023-07-15 03:16:51 +0800

    Changed cors origin

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://machakath-expense-tracker.vercel.app/",
+    origin: "https://machakath-expense-tracker.vercel.app",
   })
 );
 app.use(cookieParser());
