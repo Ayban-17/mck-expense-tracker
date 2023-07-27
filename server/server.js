8b3c982b6a00a82c326ad48e8e16fc8f454f574c
Mark Ivan <125928485+Ayban-17@users.noreply.github.com>
2023-07-28 03:53:48 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://machakath.netlify.app",
+    origin: "https://main--machakath.netlify.app",
   })
 );
 app.use(cookieParser());
