008d1dd82c506634ef162ce140ff64338d4c962b
Mark Ivan <125928485+Ayban-17@users.noreply.github.com>
2023-08-30 21:44:31 +0800

    changed proxy to netlify

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": "https://machakath-api.onrender.com",
+      "/api/v1": "https://machakath-api.netlify.app",
     },
   },
   plugins: [react()],
